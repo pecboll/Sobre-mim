6d224e4d3101e651a5cfb43cb83ce6f8b3031953
Igor Rocha <pecboll@gmail.com>
2021-02-21 20:17:59 -0300

    inicio
    inacabada

---
+++ Igor/pagina1.js
@@ -0,0 +1,21 @@
+var txtn = document.getElementById('lisn')
+var txtl = document.getElementById('lisl')
+
+function nomear(el) {
+    let disp = document.getElementById('pn').style.display;
+    if (disp == "none") {
+        let disp = document.getElementById('pn').style.display = 'block'
+    } else {
+        let disp = document.getElementById('pn').style.display = 'none'
+    }
+    function datar(el) {
+        let disp = document.getElementById('pn').style.display;
+        if (disp == "none") {
+            let disp = document.getElementById('pn').style.display = 'block'
+        } else {
+            let disp = document.getElementById('pn').style.display = 'none'
+        }
+    //let nome = ('Igor Rocha de Azevedo')
+    //txtn.innerHTML = `${nome}`
+
+}
